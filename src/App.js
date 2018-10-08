b3e4b01f9affdb80a7eb4a90ca09808ffe0fabc2
Matt Peters <matthew.george.peters@gmail.com>
2018-10-08 19:10:25 -0400

    added didMount api grab

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import SearchBooks from './comp/SearchBooks'
 import ListBooks from './comp/ListBooks'
 
 class BooksApp extends React.Component {
+  state = {
+    bookList: [],
+    showSearchPage: false
+  }
+
+  componentDidMount(){
+    BooksAPI.getAll().then((bookList) => {
+      this.setState({ bookList })
+      console.log(this.state.bookList);
+    })
+  }
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <SearchBooks/> //search
-            //<Books/>
+          <SearchBooks/>
         ) : (
-          <ListBooks/> //main
-            //<Books/>
+          <ListBooks/>
         )}
       </div>
     )
@@ -22,4 +31,3 @@ class BooksApp extends React.Component {
 }
 
 export default BooksApp
-  
